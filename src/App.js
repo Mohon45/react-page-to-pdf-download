927a6cf7fbc4e4dde2ff7de12d2d0ddc1a7e72cc
Md: Mohon <mdmohon6145@gmail.com>
2022-12-13 04:00:27 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 
 function App() {
+  const printPDF = () => {
+    const domElement = document.getElementById("download-pdf");
+    html2canvas(domElement, {
+      onclone: (document) => {
+        document.getElementById("print").style.visibility = "hidden";
+      },
+    }).then((canvas) => {
+      var imgData = canvas.toDataURL("image/png");
+      var imgWidth = 210;
+      var pageHeight = 295;
+      var imgHeight = (canvas.height * imgWidth) / canvas.width;
+      var heightLeft = imgHeight;
+
+      var doc = new jsPDF("p", "mm");
+      var position = 0;
+
+      doc.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+      heightLeft -= pageHeight;
+
+      while (heightLeft >= 0) {
+        position = heightLeft - imgHeight;
+        doc.addPage();
+        doc.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+      }
+      doc.save("welfare-application-database.pdf");
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <button id="print" onClick={printPDF}>
+        PRINT
+      </button>
+      <div id="download-pdf">
+        <h1>Genarete React to Pdf</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti
+          quasi quibusdam tempore distinctio asperiores labore repudiandae rem
+          eum culpa officiis?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
